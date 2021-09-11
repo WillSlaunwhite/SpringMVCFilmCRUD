608edd215089d1eb357001c949cdc8f2e82d56db
Will Slaunwhite <williamslaunwhite@gmail.com>
2021-09-11 12:31:13 -0600

    DAO changes

--- src/main/java/com/skilldistillery/film/dao/InMemoryFilmJdbcDaoImpl.java
+++ src/main/java/com/skilldistillery/film/dao/InMemoryFilmJdbcDaoImpl.java
@@ -3,20 +3,71 @@ package com.skilldistillery.film.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.skilldistillery.film.entities.Actor;
 import com.skilldistillery.film.entities.Film;
 
 public class InMemoryFilmJdbcDaoImpl implements FilmDAO{
 	List<Film> films = new ArrayList<>();
+	
+//	public Film(String title, String description, int releaseYear, int languageId, int rentalDuration,
+//	double rentalRate, int length, double replacementCost, String rating)
 
 	InMemoryFilmJdbcDaoImpl() {
+		films.add(new Film());
+		films.add(new Film());
+		films.add(new Film());
+		films.add(new Film());
+		films.add(new Film());
 	}
 	
 	
+
 	@Override
-	public Film findById(int filmId) {
-		
-		
+	public Film findFilmById(int filmId) {
+		// TODO Auto-generated method stub
 		return null;
+	}
+
+
+	@Override
+	public List<Film> findFilmByKeyword(String filmKeyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Actor findActorById(int actorId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<Actor> findActorsByFilmId(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Film addFilm(Film film) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public boolean deleteFilm(Film film) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean updateFilm(Film film) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
